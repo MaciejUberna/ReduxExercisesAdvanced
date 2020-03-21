99644b1f4ba8f0c7429e201aaac32107d33e13c3
Maciej Uberna <maciej.uberna@gmail.com>
2020-03-21 17:47:25 +0100

    Lesson314 "Adding Middleware" added loger to the redux store in "index.js" file.

--- adding-middleware/src/index.js
+++ adding-middleware/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import {createStore,combineReducers} from 'redux';
+import {createStore,combineReducers,applyMiddleware} from 'redux';
 import { Provider } from 'react-redux';
 
 import reducerCounter from './Store/Reducers/counter';
@@ -15,7 +15,18 @@ const rootRecucer = combineReducers({
     res: reducerResult
 });
 
-const store = createStore(rootRecucer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dsispatching: ',action);
+            const result = next(action);
+            console.log('[Middlewere] next state',store.getState());
+            return result;
+        }
+    }
+}
+
+const store = createStore(rootRecucer,applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
 registerServiceWorker();
